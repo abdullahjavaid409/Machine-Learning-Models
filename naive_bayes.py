8869243fbf31572bf1ba45f2972bf981b187f469
Syed Arsalan Amin <fahadamin738906@gmail.com>
2021-02-24 16:03:30 +0500

    Generating data from sklearn and model building

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,6 +1,17 @@
+# Importing necessary libraries
+from sklearn.naive_bayes import GaussianNB
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+sns.set()
 
 
-X, y = make_blobs(100, 2, centers=2, )
+# Making data
+
+X, y = make_blobs(100, 2, centers=2)
 plt.scatter(X[:, 0], X[:, 1])
+X[:, 0]
+# model building
+model = GaussianNB()
+model.fit(X, )
