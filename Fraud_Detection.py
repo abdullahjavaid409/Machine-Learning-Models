da781282a181f75852e1c3f556da4309bbb393c7
Syed Arsalan Amin <fahadamin738906@gmail.com>
2021-02-26 12:33:32 +0500

    Visualizing input data 'Class' column.

--- Fraud_Detection.py
+++ Fraud_Detection.py
@@ -1,3 +1,6 @@
+"""Here we'll explore the dataset for the credit card faraud detection using naive bayes.
+And predict what will the fraudelent transactions!"""
+
 # importing necessary libraries
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -10,5 +13,15 @@ df.shape
 df.describe()
 df.info
 
-
-df.Class.value_counts()  # gives the number of 1's and 0's in the the class column
+val_count = df.Class.value_counts()  # gives the number of 1's and 0's in the the class column
+# visualizing class column
+# Class column specifies that the traction belong to which class
+# 1 = Fraudelent transaction; 0 = Non-fradulaent  transaction
+print("Class column:")
+fig, ax = plt.subplots(1, 1)
+# here "%1.1f%%" says represents width of 1 and precision of 1 in the output
+ax.pie(val_count, explode=[0, 0.1], autopct='%1.1f%%',
+       labels=['Not fraud', 'Fraud'], colors=['orange', 'blue'])
+plt.axis('equal')
+# print(plt.pie.__doc__)
+# ----------------
